405d8e3095c4d27828607d22c9d62c1b98e3faf2
Prateekk02 <prateek.kumar2002@gmail.com>
2024-05-31 19:51:40 +0530

    Created model for blogContent

--- Holocron/blog/models.py
+++ Holocron/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class blogContent(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=10000)
+    author = models.CharField(max_length=100)
+    time = models.DateTimeField(default=timezone.now())
+    
+    def __str__(self) :
+        return self.name
