a0b26136a6b81a9d4b42111f0cb85be131f796ef
Prateekk02 <prateek.kumar2002@gmail.com>
2024-05-31 20:33:30 +0530

    Configured tailwind and hot reload

--- Holocron/blog/views.py
+++ Holocron/blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is home page")
+    return render(request, 'blog/home.html')
